2a7d6006fe47dea80665bd534d72f8140343aa14
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-06-16 16:20:40 -0700

    lab two started

--- server.js
+++ server.js
@@ -14,11 +14,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));  
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.get('/', getBooks);
+app.get('/add', showBook);
+// app.post('/add', addBook);
+app.get('/books/book_id', getBook);
 
 //Routes
 app.get('/', (request, response) => {
   response.render('pages/searches/index.ejs');
 });
+
+app.get('/*', (request, response) => {
+  response.status(200).send('sorry cannot find that route')
+})
 
 app.get('/booksearch', (request, response) => {
   response.render('pages/searches/new.ejs')
@@ -35,6 +43,56 @@ app.get('/searches', (request, response) => {
 // app.get('/new', (request, response) => {
 //   response.render('./')
 // })
+
+function getBook (request, response){
+  // got to the database, get a specific book using an id and show details of that specific book
+  // first we are going to have to get the id from the url - request.params
+  // go into the database using that id to find that task
+  // display that task on its own ejs page
+  console.log('this is my request.params - hopefully this is my id:', request.params);
+  let id = request.params.id;
+
+  let sql = 'SELECT * FROM books WHERE id = $1';
+  let safeValue = [id];
+
+  client.query(sql, safeValue);
+    .then (sqlResults => {
+      console.log(sqlResults.rows);
+
+      response.status(200).render('index.ejs', {book: sqlResults.rows[0]});
+    }
+      )
+      catch (err) 
+    } 
+
+
+
+function getBooks (request, response){
+  //get all of the tasks from my databse and display them on my index.ejs page
+  let sql = 'SELECT * FROM books;'
+  client.query(sql)
+    .then(sqlResults => {
+      let tasks = sqlResults.rows;
+      response.status(200).render('index.ejs', {myBooks: books})
+    })
+}
+function showBook(request, response) {
+  //display the add form
+  response.status(200).render('add.ejs')
+}
+
+// function addBook(request, response) {
+//   //collect information from the form and add it to the database
+//   let {image, title, author, description, isbn} = request.body; 
+//   let sql = 'INSERT into BOOKS (image, title, author, description, isbn) VALUES ($1, $2, $3, $4, $5) RETURNING ID;';
+//   let safeValue = [image, title, author, description, isbn];
+//   client.query(sql, safeValue);
+//     .then(results => {
+//       console.log(results.rows);
+//       response.redirect('/')
+//       response.redirect(`/books/${id}`);
+//     }
+//   }).catch();
 
 app.post('/searches', (request, response) => {
   // console.log(request.body.search);
@@ -54,11 +112,11 @@ app.post('/searches', (request, response) => {
 
   superagent.get(url)
     .then(results => {
-      console.log(results.body.items);
+      // console.log(results.body.items);
       let bookArray = results.body.items;
       const finalBookArray = bookArray.map(book => {
-        console.log(book.volumeInfo.authors);
-        console.log(book.volumeInfo.imageLinks);
+        // on
+        console.log(book.volumeInfo.industryIdentifiers[0].identifier);
         return new Book(book.volumeInfo)
       });
 
@@ -68,7 +126,6 @@ app.post('/searches', (request, response) => {
     })
 })
 
-
 // HELPER FUNCTION
 function Book(info) {
   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
@@ -77,9 +134,13 @@ function Book(info) {
   this.title = info.title ? info.title : 'no title available';
   this.author = info.author ? info.author : 'no author available';
   this.description = info.description ? info.description : 'no description available';
-
+  this.isbn = info.industryIdentifiers[0].identifier ? info.industryIdentifiers[0].identifier : 'no isbn available'
 }
 
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-})
+client.on('error', err => console.log(err));
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`);
+    })
+  })
