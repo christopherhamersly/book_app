318a16e6319c97df3e638d1be4bc00ea6757185e
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-06-17 18:19:29 -0700

    book_app

--- server.js
+++ server.js
@@ -1,42 +1,131 @@
 'use strict';
 
-
 const express = require('express');
 const superagent = require('superagent');
 require('ejs');
 require('dotenv').config();
-
-
+const pg = require('pg');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const client = new pg.Client(process.env.DATABASE_URL);
 
-
-app.use(express.urlencoded({ extended: true }));  
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 //Routes
-app.get('/', (request, response) => {
-  response.render('pages/searches/new.ejs');
-});
+app.get('/', getBooks); // rendering our home page which shows all our saved books from our database
+app.get('/add', showSearchForm); // shows our search form
+app.get('/books/:id', getOneBook); // shows our detail page
+app.post('/searches', getBooksFromAPI); // takes in our search form information and displays our show.ejs page - results from our API
+app.post('/details', addToFavorites); // takes form data from the API and adds a book to our database and then redirects to a detail page
+app.put('/update/:bookId', updateBook );
+app.delete('/delete/:bookId', deleteBook);
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('hello from the backend')
-});
+app.get('*', (request, response) => {
+  response.status(200).send('sorry cannot find that route')
+})
 
-app.get('/searches', (request, response) => {
-  response.render('pages/searches/show.ejs');
-});
+function updateBook(request,response){
+let bookId = request.params.bookId;
+let {image, title, author, description, isbn} = request.body;
+let sql = 'UPDATE books SET image=$1, title=$2, author=$3, description=$4, isbn=$5 WHERE id=$6;';
+let safeValues = {image, title, author, description, isbn, bookId};
 
-// app.get('/new', (request, response) => {
-//   response.render('./')
-// })
+client.query(sql, safeValues)
+.then(sqlResults => {
+  console.log('sqlResults',sqlResults);
+  response.redirect(`/update/${bookId}`);
+}).catch(error => console.log(error))
+}
 
-app.post('/searches', (request, response) => {
+function deleteBook(request, response){
+  let bookId = request.params.bookId;
+  let{image, title, author, description, isbn} = request.body;
+  let sql = "UPDATE book_app DELETE image=$1 title=$2 author=$3 description=$4 isbn=$5 WHERE id=$6";
+  let safeValues = [bookId];
+
+  client.query(sql, safeValues)
+.then(sqlResults => {
+  console.log('sqlResults',sqlResults);
+  response.redirect(`/update/${bookId}`);
+}).catch(error => console.log(error))
+} 
+
+
+function getBooks(request, response){
+  // go into the database, get all the books and put them on the index.ejs page
+  //do a SQL query 
+  let sqlQuery = 'SELECT * FROM books;';
+  return client.query(sqlQuery)
+    .then(resultsFromDatabase => {
+      // if there is nothing there, we go to the search page
+      if(resultsFromDatabase.rowCount === 0){
+        response.status(200).render('pages/searches/new.ejs');
+        // otherwise, we go to the index where we see all the favorite books
+      } else {
+        response.render('pages/searches/index.ejs', {bookObject: resultsFromDatabase.rows});
+        //sending forward an object called bookObject
+      }
+    }).catch(error => console.log(error))
+}
+
+function addToFavorites(request, response){
+  //collect the information from the form
+  console.log('information from my form', request.body);
+
+  let {title, authors, description, image_url, isbn} = request.body;
+
+  // put the database
+
+  let sql = 'INSERT INTO books (title, author, description, isbn, image) VALUES ($1, $2, $3, $4, $5) RETURNING id;';
+
+  let safeValue = [title, authors, description, isbn, image_url];
+
+  client.query(sql, safeValue)
+    .then(results => {
+      console.log('results from postgres', results.rows);
+      // [ { id: 3 } ]
+      let id = results.rows[0].id;
+      // redirect to the detail page
+      response.status(200).redirect(`/books/${id}`);
+    })
+}
+
+function getOneBook (request, response){
+  // got to the database, get a specific book using an id and show details of that specific book
+  // first we are going to have to get the id from the url - request.params
+  // go into the database using that id to find that task
+  // display that task on its own ejs page
+  // console.log('this is my request.params - hopefully this is my id:', request.params);
+  let id = request.params.id; // 2
+
+  let sql = 'SELECT * FROM books WHERE id = $1;';
+  let safeValue = [id]; // [2]
+
+  client.query(sql, safeValue)
+    .then (sqlResults => {
+
+      console.log('my sql results', sqlResults.rows);
+      // [ { id: 1,
+      //   image: 'http://books.google.com/books/content?id=EJUwetZIMZcC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+      //   title: 'To Kill a Mockingbird Study Guide and Student Workbook (Enhanced Ebook)',
+      //   author: 'no author available',
+      //   description: 'no description available',
+      //   isbn: '9781609339005' } ]
+
+      response.status(200).render('pages/searches/details.ejs', {bookObject2: sqlResults.rows[0]});
+
+    });
+}
+
+function showSearchForm(request, response) {
+  //display the add form
+  response.status(200).render('pages/searches/new.ejs')
+}
+
+function getBooksFromAPI(request, response){
   // console.log(request.body.search);
-  // { search: [ 'the great gatsby', 'title' ] }
-  // { search: [ 'jeff noon', 'author' ] }
-
   let query = request.body.search[0];
   let titleOrAuthor = request.body.search[1];
 
@@ -47,35 +136,39 @@ app.post('/searches', (request, response) => {
   }else if(titleOrAuthor === 'author'){
     url+=`+inauthor:${query}`;
   }
-
   superagent.get(url)
     .then(results => {
-      console.log(results.body.items);
+      // console.log(results.body.items);
       let bookArray = results.body.items;
       const finalBookArray = bookArray.map(book => {
-        console.log(book.volumeInfo.authors);
-        console.log(book.volumeInfo.imageLinks);
+        // on
+        // console.log(book.volumeInfo.industryIdentifiers[0].identifier);
         return new Book(book.volumeInfo)
-      });
-
-      console.log(finalBookArray)
-
+      })
+      
+      // console.log(finalBookArray)
+      
       response.render('pages/searches/show.ejs', {searchResults: finalBookArray})
     })
-})
-
-
+    .catch(err => {
+      console.error('ERROR', err);
+    })
+}
 // HELPER FUNCTION
 function Book(info) {
-  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-
+  // const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
   this.image = info.imageLinks.thumbnail ? info.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
   this.title = info.title ? info.title : 'no title available';
   this.author = info.author ? info.author : 'no author available';
   this.description = info.description ? info.description : 'no description available';
-
+  this.isbn = info.industryIdentifiers[0].identifier ? info.industryIdentifiers[0].identifier : 'no isbn available'
 }
 
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-})
+client.on('error', err => console.log(err));
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`);
+    })
+  })
+
